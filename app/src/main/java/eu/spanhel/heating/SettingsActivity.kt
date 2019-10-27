8c570375d2ad77d9a8d7d73441f1773e1776e960
Jan Spanhel <spainhell@gmail.com>
2019-10-27 10:18:51 +0100

    "settings activity" added

---
+++ app/src/main/java/eu/spanhel/heating/SettingsActivity.kt
@@ -0,0 +1,12 @@
+package eu.spanhel.heating
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class SettingsActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_settings)
+    }
+}
